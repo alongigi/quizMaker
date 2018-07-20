7254a4196a7ab0309c8ece4546cc9d555ca5cce3
alongigi <giga@post.bgu.ac.il>
2018-07-20 22:17:49 +0300

    create questions

--- CreateQuestions.py
+++ CreateQuestions.py
@@ -3,11 +3,13 @@ import re
 
 class CreateQuestions:
     def create_questions(self, text):
-        q1 = self.when_questions(text)
-        return q1
+        questions = {}
+        self.when_questions(text, questions)
+        self.how_much_questions(text, questions)
+        self.where_questions(text, questions)
+        return questions
 
-    def when_questions(self, text):
-        qa = {}
+    def when_questions(self, text, questions):
         for i, x in enumerate(text):
             if x.isdigit() and len(x) == 4:
                 j = i - 1
@@ -16,15 +18,57 @@ class CreateQuestions:
                     t = text[j] + " " + t
                     j = j-1
                 t = "When does " + t
-                if len(t.split()) > 7:
-                    qa[t] = text[i]
+                if len(t.split()) > 5:
+                    questions[t] = text[i]
                 else:
                     j = i + 1
                     t = "When does"
-                    while text[j] != "." and text[j] != ",":
+                    while j <= len(text) and text[j] != "." and text[j] != ",":
                         t = t + " " + text[j]
                         j = j + 1
                     t = t + " ?"
-                    if len(t.split()) > 7:
-                        qa[t] = text[i]
-        return qa
+                    if len(t.split()) > 5:
+                        questions[t] = text[i]
+
+    def how_much_questions(self, text, questions):
+        for i, x in enumerate(text):
+            if x.isdigit() and len(x) != 4:
+                j = i - 1
+                t = "?"
+                while j >= 0 and text[j] != "." and text[j] != ",":
+                    t = text[j] + " " + t
+                    j = j-1
+                t = "How much does " + t
+                if len(t.split()) > 5:
+                    questions[t] = text[i]
+                else:
+                    j = i + 1
+                    t = "How much does"
+                    while j <= len(text) and text[j] != "." and text[j] != ",":
+                        t = t + " " + text[j]
+                        j = j + 1
+                    t = t + " ?"
+                    if len(t.split()) > 5:
+                        questions[t] = text[i]
+
+    def where_questions(self, text, questions):
+        for i, x in enumerate(text):
+            if x == "in" and text[i+1].isupper():
+                j = i - 1
+                t = "?"
+                while j >= 0 and text[j] != "." and text[j] != ",":
+                    t = text[j] + " " + t
+                    j = j-1
+                t = "Where does " + t
+                if len(t.split()) > 5:
+                    questions[t] = text[i+1]
+                else:
+                    j = i + 1
+                    t = "Where does"
+                    while j <= len(text) and text[j] != "." and text[j] != ",":
+                        t = t + " " + text[j]
+                        j = j + 1
+                    t = t + " ?"
+                    if len(t.split()) > 5:
+                        questions[t] = text[i+1]
+
